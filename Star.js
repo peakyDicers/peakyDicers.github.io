a7a4a65f9fe3442b2aa73980137b9af60ec84585
Ian Chui <ian.chui@hotmail.com>
2018-06-19 21:43:15 -0400

    new background

---
+++ Star.js
@@ -0,0 +1,9 @@
+class Star{
+    constructor(x, y, z, width, height){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.width = width;
+        this.height = height;
+    }
+}
