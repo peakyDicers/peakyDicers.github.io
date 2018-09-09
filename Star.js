1e95e9e044a5575c8dcc25a4d7cac719f31a9260
peakyDicers <ian.chui@hotmail.com>
2018-09-09 02:31:37 -0400

    reborn

--- Star.js
+++
@@ -1,9 +0,0 @@
-class Star{
-    constructor(x, y, z, width, height){
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.width = width;
-        this.height = height;
-    }
-}
